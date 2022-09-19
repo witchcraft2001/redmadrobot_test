9300861327b6705ef80a1df3287d8916f436e932
DmitryM <mikhaltchenkov@gmail.com>
2022-09-19 20:03:55 +0300

    Initial commit

---
+++ src/main/java/com/dmdev/bootcamptest/BootcamptestApplication.java
@@ -0,0 +1,13 @@
+package com.dmdev.bootcamptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcamptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcamptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dmdev/bootcamptest/BootcamptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmdev.bootcamptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcamptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
